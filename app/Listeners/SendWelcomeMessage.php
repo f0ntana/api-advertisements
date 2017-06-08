49db5d4ae86de8cfd418262d94b05bb7bf397def
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2017-06-08 16:57:49 -0300

    Add import

--- app/Listeners/SendWelcomeMessage.php
+++ app/Listeners/SendWelcomeMessage.php
@@ -4,8 +4,6 @@ namespace App\Listeners;
 
 use App\Notifications\WelcomeMessage;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class SendWelcomeMessage
 {
